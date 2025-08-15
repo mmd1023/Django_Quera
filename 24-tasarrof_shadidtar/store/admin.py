25cf8d51752180c64074669ecd29be26b6c5763a
MMD <mmd.gh313@gmail.com>
2025-08-15 14:32:27 +0330

    add pset24

---
+++ 24-tasarrof_shadidtar/store/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'is_active']
+    list_display_links = ['id', 'name']
+    
+    fieldsets = (
+        ('Identification', {
+            'fields': ('name', 'price', 'is_active')
+        }),
+        ('Details', {
+            'classes': ('collapse',),
+            'fields': ('category', 'company')
+        }),
+    )
+
+
+    actions = ['make_inactive']
+    @admin.action(description='Make inactive')
+    def make_inactive(self, request, queryset):
+        updated = queryset.update(is_active = False)
+        self.message_user(request, f"{updated} {ngettext('product', 'products', updated)} inactivated", messages.SUCCESS)
