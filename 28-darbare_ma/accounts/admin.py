e1852bc3a7a2c9c3d4937290d8bb374a38900cc1
MMD <mmd.gh313@gmail.com>
2025-08-22 20:51:58 +0330

    add pset28

---
+++ 28-darbare_ma/accounts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = DefaultUserAdmin.list_display + ('is_active',)
+    sortable_by = ('username',)
+    list_editable = ('is_staff', 'is_active')
+
+    list_filter = ('gender', 'is_staff', 'is_superuser', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'gender', 'age', 'description')}),
+        ('Contact info', {'fields': ('phone', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
