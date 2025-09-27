eb4f8a9e1d55895ab39d3512bef2677e150de165
MMD <mmd.gh313@gmail.com>
2025-09-27 15:46:13 +0330

    add pset44

---
+++ 44-darkhast_task/charities/views.py
@@ -0,0 +1,93 @@
+from rest_framework import status, generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from accounts.permissions import IsCharityOwner, IsBenefactor
+from charities.models import Task
+from charities.serializers import (
+    TaskSerializer, CharitySerializer, BenefactorSerializer
+)
+
+
+class BenefactorRegistration(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = BenefactorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class CharityRegistration(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = CharitySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Tasks(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.all_related_tasks_to_user(self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            **request.data,
+            "charity_id": request.user.charity.id
+        }
+        serializer = self.serializer_class(data = data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            self.permission_classes = [IsAuthenticated, ]
+        else:
+            self.permission_classes = [IsCharityOwner, ]
+
+        return [permission() for permission in self.permission_classes]
+
+    def filter_queryset(self, queryset):
+        filter_lookups = {}
+        for name, value in Task.filtering_lookups:
+            param = self.request.GET.get(value)
+            if param:
+                filter_lookups[name] = param
+        exclude_lookups = {}
+        for name, value in Task.excluding_lookups:
+            param = self.request.GET.get(value)
+            if param:
+                exclude_lookups[name] = param
+
+        return queryset.filter(**filter_lookups).exclude(**exclude_lookups)
+
+
+class TaskRequest(APIView):
+    permission_classes = [IsBenefactor]
+
+    def get(self, request, task_id):
+        task = get_object_or_404(Task, pk=task_id)
+        if task.state != Task.TaskStatus.PENDING:
+            return Response(data={'detail': 'This task is not pending.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        task.assign_to_benefactor(request.user.benefactor)
+        return Response(data={'detail': 'Request sent.'}, status=status.HTTP_200_OK)
+
+
+class TaskResponse(APIView):
+    pass
+
+
+class DoneTask(APIView):
+    pass
